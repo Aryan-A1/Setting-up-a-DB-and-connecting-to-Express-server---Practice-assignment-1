556a5868a97a765568581a1fe0180398c5fb286a
Aryan-A1 <aryankumarsingh9370@gmail.com>
2025-03-23 15:27:53 +0530

    Complete

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+require('dotenv').config(); 
 const express = require('express');
+const mongoose = require('mongoose');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3010; 
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to database'))
+.catch(err => console.error(`Error connecting to database: ${err.message}`));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
